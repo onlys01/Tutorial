0112912a4490948d81aebd9553d04e21de39e668
guill <guill@PC-GUILLE>
2020-10-23 09:33:21 +0200

    Tutorial Commit

---
+++ TutorialGithub/src/tutorialgithub/TutorialGithub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorialgithub;
+
+/**
+ *
+ * @author guill
+ */
+public class TutorialGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido");
+    }
+    
+}
